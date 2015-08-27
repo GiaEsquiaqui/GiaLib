facc5c328869e990066bf6352825ff41ad66002a
Andrea Giovanna Esquiaqui Pérez <giaesquiaqui@hotmail.com>
2015-08-26 21:00:16 -0500

    Enteros.h

---
+++ Enteros/Enteros.h
@@ -0,0 +1,26 @@
+#ifndef ENTEROS_H_INCLUDED
+#define ENTEROS_H_INCLUDED
+#include <iostream>
+
+using namespace std;
+
+int factorial(int);
+int num_cifras(int);
+int gallina(int);
+int baldosines(int);
+int lados_triangulo(double, double, double);
+int NumeroPatasPatos(int);
+int NumeroPatasCaballos(int);
+int NumeroCuadradadosCuaderno(int, int, int);
+int NumeroH2o(int, int);
+int menor(int, int, int);
+int mayor(int, int, int);
+int techo(int);
+int piso(int);
+int bolitas(double, double, double, double, int);
+int ramitas(double, int, int);
+int operacion_ramitas(int, int);
+int girasoles(int, int, int, int);
+
+
+#endif // ENTEROS_H_INCLUDED
