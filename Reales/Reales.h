ffcd12baf49fe1e9e0a00412cde6f46235418499
Andrea Giovanna Esquiaqui Pérez <giaesquiaqui@hotmail.com>
2015-08-26 21:40:45 -0500

    Reales.h

---
+++ Reales/Reales.h
@@ -0,0 +1,27 @@
+#ifndef REALES_H_INCLUDED
+#define REALES_H_INCLUDED
+#include "operaciones_basicas.h"
+#include <iostream>
+
+using namespace std;
+
+double sumaperimetros(double*, int);
+double radios();
+double chavo(int);
+double circulo(double);
+double perimetroCirculo(double);
+double AreaTriangulo(double, double);
+double PerimetroTriangulo(double, double, double);
+double AreaRectangulo(double, double);
+double distancias (double, double);
+double fuerza(double, double);
+double programa_arroz_para3(double);
+double programa_arroz_paran(double, int);
+double polinomio(double, double, double, double);
+double programa_polinomio(double, double, double, double);
+double circulo_en_cuadrado(double);
+double cuadrado_en_circulo(double);
+double proyectil(double, double);
+double soldados(int, double);
+
+#endif // REALES_H_INCLUDED
